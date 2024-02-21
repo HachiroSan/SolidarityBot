dbd8bc5cd7da678e71b0c24a6382cc1f762ab65c
Hachiro <snaptrap.org@gmail.com>
2024-02-21 23:43:36 +0800

    fix: :bug: fix reference error
    - add use strict\n-fix reference error

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
+'use strict';
+
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 const { JWT } = require('google-auth-library');
 const fs = require('fs');
 const Fuse = require('../lib/fuse');
 const TelegramBot = require('node-telegram-bot-api');
-const { strict } = require('assert');
+
 
 
 const BOT_TOKEN = '6463257783:AAE1favGh3wgs0RMQw_vSuLyOrBVjhYMLqE';
 const SPREADSHEET_ID = '1vucTAdf8cSq5FisJWuWxt4nXdjRTinPQZYd8ZQ8g1rE'
 
 
-const google_authenticate = async function() {
+const google_authenticate = async function () {
   const account = JSON.parse(fs.readFileSync('ssh-vpn.json'));
 
   const serviceAccountAuth = new JWT({
@@ -22,7 +24,7 @@ const google_authenticate = async function() {
   return serviceAccountAuth;
 }
 
-const fetchSheetData = async function(doc, range) {
+const fetchSheetData = async function (doc, range) {
   const sheet = doc.sheetsByIndex[0];
 
   await sheet.loadCells(range);
@@ -30,7 +32,7 @@ const fetchSheetData = async function(doc, range) {
   let dataArray = [];
 
   for (let row = 1; row < 150; row++) {
-    data = {
+    let data = {
       "Name": sheet.getCell(row, 0).value,
       "Category": sheet.getCell(row, 1).value,
       "Reason": sheet.getCell(row, 2).value,
@@ -48,44 +50,44 @@ const fetchSheetData = async function(doc, range) {
   }
 }
 
-function findItem(itemData, searchQuery) {
+const findItem = async function(itemData, searchQuery) {
+  const fuseOptions = {
+    keys: ["Name"],
+    threshold: 0.4
+  };
 
-  // Initialize Fuse instance with itemData and search keys
-  const fuse = new Fuse(itemData, { keys: ["Name"], threshold: 0.4 });
+  const fuse = new Fuse(itemData, fuseOptions);
+  const result = await fuse.search(searchQuery);
 
-  // Search for the searchQuery
-  const result = fuse.search(searchQuery);
-
-  // If there's a match, return the matched item's details
   if (result.length > 0) {
-    const matchedItem = result[0].item;
+    const { Name, Category, Reason, Reference, Image } = result[0].item;
     return {
-      text: `Name: ${matchedItem.Name}\nCategory: ${matchedItem.Category}\nReason: ${matchedItem.Reason}\nReference: ${matchedItem.Reference}`,
-      image_url: matchedItem.Image
+      text: `Name: ${Name}\nCategory: ${Category}\nReason: ${Reason}\nReference: ${Reference}`,
+      image_url: Image
     };
   }
 
-  // If no match found, return a default message
   return {
     text: "We do not have official information or facts to indicate that this brand/company has any affiliation with Israel-linked companies or supports the Zionist occupation of Palestine.",
     image_url: null
   };
 }
 
-const loadData = async function() {
+
+const loadData = async function () {
   const serviceAccountAuth = await google_authenticate();
   const doc = new GoogleSpreadsheet(SPREADSHEET_ID, serviceAccountAuth);
   await doc.loadInfo(); // loads document properties and worksheets
   console.log("Loaded doc: " + doc.title);
-  
+
   const sheetData = await fetchSheetData(doc, 'A1:E150');
 
   console.log("Loaded sheet: " + sheetData.title);
-  console.log("Loaded cell: " + sheetData.cellStats.total);
+  console.log("Loaded cell: " + sheetData.cellStats.total + " cells\n");
 
   return sheetData.data;
 }
 
-module.exports = {loadData, findItem};
+module.exports = { loadData, findItem };
 
 
