c00fdf88574cea9f5498e2b701687b5e8fc9d702
Hachiro <snaptrap.org@gmail.com>
2024-02-21 23:42:48 +0800

    feat: :art: add error catching
    - add error catching \n- handle sendPhoto and sendMessage

--- src/tele.js
+++ src/tele.js
@@ -1,23 +1,35 @@
+'use strict';
+
 const TelegramBot = require('node-telegram-bot-api');
 const {loadData, findItem} = require('./index');
 
-const BOT_TOKEN = '6463257783:AAE1favGh3wgs0RMQw_vSuLyOrBVjhYMLqE';
 
+const BOT_TOKEN = '6463257783:AAE1favGh3wgs0RMQw_vSuLyOrBVjhYMLqE';
 const bot = new TelegramBot(BOT_TOKEN, {polling: true});
 
-sheetData = loadData();
+let sheetData;
 
+loadData().then((data) => {
+    sheetData = data;
 
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-    const username = msg.from.username;
-    const messageText = msg.text;
+    bot.on('message', async (msg) => {
+        const chatId = msg.chat.id;
+        const username = msg.from.username;
+        const messageText = msg.text;
+    
+        // Find the item in the cache
+        const response = await findItem(sheetData, messageText);
+        if (response.image_url != null) {
+            bot.sendPhoto(msg.chat.id, response.image_url, {caption: response.text})
+                .catch((error) => {
+                    bot.sendMessage(msg.chat.id, response.text);
+                });
+        } else {
+            bot.sendMessage(msg.chat.id, response.text);
+        }
 
-    // Find the item in the cache
-    const response = findItem(sheetData, messageText);
-    bot.sendMessage(msg.chat.id, response.text);
-
-    // Log the message to the console
-    const timestamp = new Date().toLocaleString();
-    console.log(`${timestamp} - Username: ${username}, Message: ${messageText}`);
+        // Log the message to the console
+        const timestamp = new Date().toLocaleString();
+        console.log(`${timestamp} - Username: ${username}, Message: ${messageText}`);
+    });
 });
