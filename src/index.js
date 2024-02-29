a4ee39a485b444fd58b27aeab3a6620273b7676e
Muhd Farhad <snaptrap.org@gmail.com>
2024-02-29 10:58:33 +0800

    refactor: :art: add comments and proper spacing

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const { GoogleSpreadsheet } = require('google-spreadsheet');
 const { JWT } = require('google-auth-library');
 const fs = require('fs');
 const Fuse = require('../lib/fuse');
-const TelegramBot = require('node-telegram-bot-api');
 
 const SPREADSHEET_ID = '1vucTAdf8cSq5FisJWuWxt4nXdjRTinPQZYd8ZQ8g1rE'
 
@@ -24,20 +23,29 @@ const google_authenticate = async function () {
 const SHEET_RANGE = process.env.SHEET_RANGE || 100;
 
 
-const fetchSheetData = async function (doc, range) {
+/**
+ * Fetches data from a Google Sheets document.
+ * @param {GoogleSpreadsheet} doc - The GoogleSpreadsheet object representing the document.
+ * @param {string} range - The range of cells to load.
+ * @returns {Object} - An object containing the title, cellStats, and data.
+ */
+const fetchSheetData = async (doc, range) => {
   const sheet = doc.sheetsByIndex[0];
 
+  // Load the specified range of cells
   await sheet.loadCells(range);
 
-  let dataArray = [];
+  const dataArray = [];
+
+  const SHEET_RANGE = sheet.rowCount;
 
   for (let row = 1; row < SHEET_RANGE; row++) {
-    let data = {
-      "Name": sheet.getCell(row, 0).value,
-      "Category": sheet.getCell(row, 1).value,
-      "Reason": sheet.getCell(row, 2).value,
-      "Reference": sheet.getCell(row, 3).value,
-      "Image": sheet.getCell(row, 4).value
+    const data = {
+      Name: sheet.getCell(row, 0).value,
+      Category: sheet.getCell(row, 1).value,
+      Reason: sheet.getCell(row, 2).value,
+      Reference: sheet.getCell(row, 3).value,
+      Image: sheet.getCell(row, 4).value,
     };
 
     dataArray.push(data);
@@ -46,14 +54,14 @@ const fetchSheetData = async function (doc, range) {
   return {
     title: sheet.title,
     cellStats: sheet.cellStats,
-    data: dataArray
-  }
-}
+    data: dataArray,
+  };
+};
 
-const findItem = async function(itemData, searchQuery) {
+const findItem = async function (itemData, searchQuery) {
   const fuseOptions = {
     keys: ["Name"],
-    threshold: 0.30
+    threshold: 0.3,
   };
 
   const fuse = new Fuse(itemData, fuseOptions);
@@ -63,31 +71,33 @@ const findItem = async function(itemData, searchQuery) {
     const { Name, Category, Reason, Reference, Image } = result[0].item;
     return {
       text: `Name: ${Name}\nCategory: ${Category}\nReason: ${Reason}\nReference: ${Reference}`,
-      image_url: Image
+      image_url: Image,
     };
   }
 
   return {
-    text: "We do not have official information or facts to indicate that this brand/company has any affiliation with Israel-linked companies or supports the Zionist occupation of Palestine.",
-    image_url: null
+    text:
+      "We do not have official information or facts to indicate that this brand/company has any affiliation with Israel-linked companies or supports the Zionist occupation of Palestine.",
+    image_url: null,
   };
-}
-
+};
 
 const loadData = async function () {
+  // Authenticate with Google service account
   const serviceAccountAuth = await google_authenticate();
-  const doc = new GoogleSpreadsheet(SPREADSHEET_ID, serviceAccountAuth);
-  await doc.loadInfo(); // loads document properties and worksheets
-  console.log("Loaded doc: " + doc.title);
 
+  // Load the Google Spreadsheet
+  const doc = new GoogleSpreadsheet(SPREADSHEET_ID, serviceAccountAuth);
+  await doc.loadInfo(); // Loads document properties and worksheets
+  console.log(`Loaded doc: ${doc.title}`);
+
+  // Fetch the data from the specified sheet range
   const sheetData = await fetchSheetData(doc, `A1:E${SHEET_RANGE}`);
 
-  console.log("Loaded sheet: " + sheetData.title);
-  console.log("Loaded cell: " + sheetData.cellStats.total + " cells\n");
+  console.log(`Loaded sheet: ${sheetData.title}`);
+  console.log(`Loaded cell: ${sheetData.cellStats.total} cells\n`);
 
   return sheetData.data;
 }
 
 module.exports = { loadData, findItem };
-
-
